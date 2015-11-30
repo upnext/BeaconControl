6165ec5d490daf6d591c9720aca6b78065359f2f
Bogusław Tolarz <btolarz@gmail.com>
2015-11-30 16:01:43 +0100

    BOS-833: Resolved domain session bug on heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_beacon_os_session', domain: :all
+if AppConfig.landing_page_url
+  Rails.application.config.session_store :cookie_store, key: '_beacon_os_session', domain: :all
+else
+  Rails.application.config.session_store :cookie_store, key: '_beacon_os_session', domain: :all
+end
