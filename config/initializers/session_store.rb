4c8c3b6aa1039e73874b078508595b5ac578785c
Bogusław Tolarz <btolarz@gmail.com>
2015-11-30 16:15:33 +0100

    BOS-833: Fixed session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
-if AppConfig.landing_page_url
+if AppConfig.landing_page_url.present?
   Rails.application.config.session_store :cookie_store, key: '_beacon_os_session', domain: :all
 else
-  Rails.application.config.session_store :cookie_store, key: '_beacon_os_session', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_beacon_os_session'
 end
