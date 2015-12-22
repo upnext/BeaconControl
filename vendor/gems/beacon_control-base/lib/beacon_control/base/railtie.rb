659b00cb4000acfafac114b3efc602c08ee2e842
Adrian Wozniak <adrian.wozniak@up-next.com>
2015-12-22 14:14:50 +0100

    BOS-FF: Fix load issue

--- vendor/gems/beacon_control-base/lib/beacon_control/base/railtie.rb
+++ vendor/gems/beacon_control-base/lib/beacon_control/base/railtie.rb
@@ -15,8 +15,35 @@ module BeaconControl
       )
 
       config.before_configuration do |app|
-        app.paths['app/models'] << File.expand_path('../../../../app/models', __FILE__)
-        app.paths['app/controllers'] << File.expand_path('../../../../app/controllers', __FILE__)
+        append_autoload_dir(File.expand_path('../../../..', __FILE__).to_s)
+        append_autoload_dir(app.root)
+        exec_autoload_dirs
+        app.paths.values
+      end
+
+      def self.append_autoload_dir(dir)
+        @after_here_exec ||= []
+        @after_here_exec.prepend dir
+      end
+
+      def self.exec_autoload_dirs
+        (@after_here_exec || []).each do |dir|
+          load_for_dir(dir)
+        end
+      end
+
+      def self.load_for_dir(dir)
+        config.before_configuration do |app|
+          Dir["#{dir}/app/*"].each do |path|
+            /(?<p>app\/\w+)$/ =~ path.to_s
+            next unless defined?(p) and p.present?
+            next if /(views|assets)$/ =~ p
+            if app.paths[p].blank?
+              app.paths.add p, eager_load: true, load_path: true
+            end
+            app.paths[p] << path
+          end
+        end
       end
 
       config.after_initialize { BeaconControl::Base.exec_load }
